a642735e23d151e4e2ce4754e83ddbaf3c0c9f00
ansarali70 <ansar.ali4018@gmail.com>
2018-03-08 15:22:49 +0500

    Ansar

---
+++ app/src/main/java/project/itpvt/com/mohagni/Activities/All_Products.java
@@ -0,0 +1,15 @@
+package project.itpvt.com.mohagni.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import project.itpvt.com.mohagni.R;
+
+public class All_Products extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all__products);
+    }
+}
---
+++ app/src/main/java/project/itpvt/com/mohagni/Activities/Home.java
@@ -0,0 +1,15 @@
+package project.itpvt.com.mohagni.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import project.itpvt.com.mohagni.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
